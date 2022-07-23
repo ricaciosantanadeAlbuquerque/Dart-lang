6f11fc2674dedf941c2f7dd70e2610352fdd6816
ricacio <ricaciozz@gmail.com>
2022-07-23 15:12:27 -0300

    23-07-2022

---
+++ Exercícios/laço_de repetição.dart
@@ -0,0 +1,19 @@
+void main(){
+  List<String> palavras = ["Lucas","Joao","maria","mathues","marta"];
+
+  for(int i = 0; i < palavras.length;i++){
+    print('${palavras[i]}');
+  }
+  /*
+  String
+  int
+  bool
+  double
+  List<>
+  maps
+  Iterable
+  Object
+  num
+  dynamic
+   */
+}
