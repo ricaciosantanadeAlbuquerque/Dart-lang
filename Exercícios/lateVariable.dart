b47fe0cb25a4cfd70eaca8491b11ef0be7720eb9
ricacio <ricaciozz@gmail.com>
2022-07-23 15:09:32 -0300

    23-07-2022

---
+++ Exercícios/lateVariable.dart
@@ -0,0 +1,12 @@
+late int valor; // o operador late permite declarar a variável fora da função como variável de classe ou superiror, permite ao programador inicializar está variável dentro de uma função; 
+
+void main(){
+// Opreador Late
+//int valor; // variável não nula declarada
+ 
+// valor = 1433; // variável inicializada
+  valor = 15; 
+
+  print(valor); // variável sendo utilizada
+
+}
