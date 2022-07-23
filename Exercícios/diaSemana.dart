f6fd9d6f818998d80838931e7e90c79ccf7f082f
ricacio <ricaciozz@gmail.com>
2022-07-23 15:17:14 -0300

    23-07-2022

---
+++ Exercícios/diaSemana.dart
@@ -0,0 +1,41 @@
+import 'dart:io';
+
+void main() {
+  print("======MEUNU======");
+  print("Digite [1] para Domingo.");
+  print("Digite [2] para Segunda. ");
+  print("Digite [3] para terça ");
+  print("Digite [4] para quarta ");
+  print("Digite [5] para quinta ");
+  print("Digite [6] para sexta ");
+  print("Digite [7] para Sábado.");
+
+  String? op = stdin.readLineSync();
+  int op1 = int.parse(op!);
+  switch (op1) {
+    case 1:
+      print("Domingo.");
+      break;
+    case 2:
+      print("Segunda.");
+      break;
+    case 3:
+      print("Terça");
+      break;
+    case 4:
+      print("Quarta");
+      break;
+    case 5:
+      print("Quinta");
+      break;
+    case 6:
+      print("Sexta");
+      break;
+    case 7:
+      print("Sábado");
+      break;
+    default:
+      print("Valor fora da faixa");
+  }
+  print("Fim programa !");
+}
