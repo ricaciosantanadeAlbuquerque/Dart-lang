5d082c98b35a89a3b87d731d57969c47e27f150e
ricacio <ricaciozz@gmail.com>
2022-07-23 15:11:38 -0300

    23-07-2022

---
+++ Exercícios/variavel.dart
@@ -0,0 +1,28 @@
+void main(){
+  /*
+  VARIÁVEIS DO TIPO NULO
+  *Object?
+  * double?
+  * num?
+  *int?
+  * String?
+  *Iterable?
+  * bool?
+  * list?
+
+   */
+  /*
+  VARIÁVEIS DO TIPO NÃO NULO
+  *Objetc
+  *double
+  *num
+  *int
+  *String
+  *Iterable
+  *bool
+  *list
+   */
+  
+
+  // podemos dizer que a ?  seria um operador  nullo, que torna a variável do tipo não nula em nula.
+}
