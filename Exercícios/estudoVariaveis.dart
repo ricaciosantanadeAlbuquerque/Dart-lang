de20b1ef01e6c1d952b15ff89b2147936352dc5f
ricacio <ricaciozz@gmail.com>
2022-07-23 15:02:31 -0300

    23-07-2022

---
+++ Exercícios/estudoVariaveis.dart
@@ -0,0 +1,30 @@
+
+void main() {
+  // Declaração e inicialização de variáveis
+  String nome = "fullter";
+
+  int valor = 1500;
+
+  bool verdade = true;
+
+  List<String> listaNome = ["Joao", "maria"];
+
+  // lendo as variáveis;
+
+  print(nome);
+  print(valor);
+  print(verdade);
+  print('${listaNome[0]} - ${listaNome[1]}');
+
+  //=======================================================================
+
+  int num1 = 0;
+  double num2 = 0.0;
+  String num3 = "MegaDramon";
+  bool v = true;
+  
+  print(num1);
+  print(num2);
+  print(num3);
+  print(v);
+}
