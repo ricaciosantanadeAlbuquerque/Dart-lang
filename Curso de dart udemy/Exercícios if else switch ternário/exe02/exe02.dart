0643dce192b95c1b90db6a2a104b23068c4c0fa0
ricacio <ricaciozz@gmail.com>
2022-12-19 16:00:35 -0300

    19-12-2022

--- Curso de dart udemy/Exercícios if else switch ternário/exe02/exe02.dart
+++ Curso de dart udemy/Exercícios if else switch ternário/exe02/exe02.dart
@@ -4,4 +4,17 @@
  */
 import 'dart:io';
 
-void main() {}
+void main() {
+  print("Digite por favor seu ano de nascimento ");
+  String? entrada = stdin.readLineSync();
+  int anoNascimento = int.parse(entrada!);
+  int idade = 2022 - anoNascimento;
+
+  if (idade < 16) {
+    print(" Você não pode votar ");
+  } else if ((idade >= 16 && idade < 18) || (idade > 70)) {
+    print("O voto é opcional ");
+  } else {
+    print("voto obrigatório ");
+  }
+}
