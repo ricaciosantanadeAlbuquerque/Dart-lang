226de426240a964ba63bb3161b801c7f43b84c80
ricacio <ricaciozz@gmail.com>
2023-03-03 14:32:15 -0300

    03-03-2023

---
+++ Curso de dart udemy/revisaoVetores08/main.dart
@@ -0,0 +1,60 @@
+/**
+ *  Ler as notas finais de uma turma de 10 alunos armazenando-as em um vetor N. A seguir calcule a
+ * média aritmética das notas dos alunos aprovados (nota maior ou igual a 6). Armazene em um vetor P a
+ * posição (índice) que cada aluno ocupa no vetor N, dos alunos que obtiveram nota maior que a média
+ * calculada. Imprimir a média calculada e logo após o vetor P.
+ * OBS: Não deixar valores em branco entre os elementos de P
+ */
+import 'dart:io';
+
+void main() {
+  List<String> nome = [];
+  List<double> N = [];
+  List<double> p = [];
+
+  for (var i = 0; i < 10; i++) {
+    
+    for (var j = 0; j < 1; j++) {
+      print("Digite o seu nome !");
+      String? entradaNome = stdin.readLineSync();
+      print("Digite sua primeira nota !");
+      String? num1 = stdin.readLineSync();
+      print("Digite sua segunda nota ! ");
+      String? num2 = stdin.readLineSync();
+      print("Digite sua terceira nota ! ");
+      String? num3 = stdin.readLineSync();
+
+      if (entradaNome != null && num1 != null && num2 != null && num3 != null) {
+        if (entradaNome.isNotEmpty && num1.isNotEmpty && num2.isNotEmpty && num3.isNotEmpty) {
+          nome.add(entradaNome);
+          try {
+            double nota1 = double.parse(num1);
+            double nota2 = double.parse(num2);
+            double nota3 = double.parse(num3);
+
+            double soma = nota1 + nota2 + nota3;
+            N.add(soma);
+          } catch (NumberFormetException) {
+            print("Digite apenas valores numericos ");
+          }
+        } else {
+          print("Digite apenas valores válidos ! ");
+        }
+      }
+    }
+  }
+  print(N);
+
+  for (var notaF in N) {
+    double media = notaF / 3;
+
+    if (media >= 6) {
+      p.add(media);
+    }
+  }
+  print(p);
+  for (var i = 0; i < p.length; i++) {
+    //0 a 2
+    print("Parabéns Aluno(a) ${nome[i]} você está aprovada! com a média ${p[i].truncate()}");
+  }
+}
