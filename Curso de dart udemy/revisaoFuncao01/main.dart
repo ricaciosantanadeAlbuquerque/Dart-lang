df4b694ce99f40b52e736046ad4c72056f3c1e7d
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-03-08 16:17:06 -0300

    08-03-2023

---
+++ Curso de dart udemy/revisaoFuncao01/main.dart
@@ -0,0 +1,18 @@
+/**
+ *  Faça uma função que recebe a idade de uma pessoa em anos, meses e dias e retorna essa idade expressa em dias.
+ */
+void main() {
+  print("${idadeDia(dia: 21, mes: 10, ano: 1992)} dias  de vida na terra ");
+}
+
+String idadeDia({required int dia, required int mes, required int ano}) {
+  double resultadoMes = mes * 30.437;
+  int idade = DateTime.now().year - ano;
+  //print(idade);
+  double resultadoAnos = idade * 365;
+  double diaResposta = resultadoMes + resultadoAnos + dia;
+
+  return diaResposta.toStringAsFixed(0);
+  //return resultadoAnos.toString();
+  //return resultadoMes.toStringAsFixed(0);
+}
