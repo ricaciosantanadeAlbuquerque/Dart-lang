b3404ae7c0b494eec888587fb4baa2b6d2f59ae8
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-03-20 20:00:47 -0300

    20-03-2023

---
+++ Curso de dart udemy/revisaoFuncao12/main.dart
@@ -0,0 +1,15 @@
+/**
+ *  Média 
+ */
+void main() {
+  var media1 = media();
+  print(media1);
+  media1(10.0, 10.0, 10.0, 10.0);
+}
+
+Function media = () {
+  return (double a, double b, double c, double d) {
+    var mensagem = (a + b + c + d) / 4;
+    print(mensagem);
+  };
+};
