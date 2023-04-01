496400f8c91965a69a4abaf4ac1737c18bd6ff06
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-04-01 16:50:43 -0300

    01-04-2023

---
+++ Curso de dart udemy/revisaoConstrutores01/main.dart
@@ -0,0 +1,7 @@
+import '../Construtores/main.dart';
+
+void main() {
+  Objeto objeto = Objeto();
+  objeto.nome = " Carro";
+  print("O nome do objeto é ${objeto.nome}");
+}
---
+++ Curso de dart udemy/revisaoConstrutores01/objeto.dart/objeto.dart
@@ -0,0 +1,8 @@
+class Objeto {
+
+  String? nome; // atribulto
+
+  Objeto() { // construtor sem parâmetros
+    print("Construtor sem parametros! \n${this.toString()}"); // this. é a instância  da classe Objeto
+  }
+}
