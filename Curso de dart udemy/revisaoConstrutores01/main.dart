4e652698cd47d84ceb80b6c88950e7a605040591
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-04-01 16:56:17 -0300

    01-04-2023

--- Curso de dart udemy/revisaoConstrutores01/main.dart
+++ Curso de dart udemy/revisaoConstrutores01/main.dart
@@ -1,7 +1,7 @@
 import '../Construtores/main.dart';
 
 void main() {
-  Objeto objeto = Objeto();
-  objeto.nome = " Carro";
-  print("O nome do objeto é ${objeto.nome}");
+  Objeto objeto = Objeto(); // linha de instânciamento
+  objeto.nome = " Carro";   // acessando  diretamente o atribulto.
+  print("O nome do objeto é ${(objeto.nome != null) ? objeto.nome : ' Valor indefinido !'}");
 }
