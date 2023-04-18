bfa8f5ef232c3b5e401aed19bce84e12229c7720
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-04-18 01:00:59 -0300

    18-04-2023

--- Curso de dart udemy/static03/main.dart
+++ Curso de dart udemy/static03/main.dart
@@ -7,9 +7,20 @@ void main() {
   print("${calculo.pi}\n"); // Atravez do objeto calculo posso acessar os atribultos e métodos de instância da classe
 
   print(calculo.areaCirculo(raio));
-   print("");
+  print("");
   // atribultos e métodos de classe, pertencem a classe, são visíveis a classe e não a o objeto da classe.
   print(Calculo2.piEstatico);
   print("");
   print(Calculo2.areaCirculoEstatic(raio));
+
+  Calculo2 calculo1 = Calculo2();
+  print(calculo1.pi);
+  print(calculo1.areaCirculo(raio));
+  // comparando objetos ===========================
+  print("Instancias consomem: ${identical(calculo,calculo1)? 'menos memória pois são iguais' : 'Mais memória pois são diferentes'}");
+  print('');
+  print("\n Retornos iguais?: ${(Calculo2.areaCirculoEstatic(raio) == Calculo2().areaCirculo(raio) ? 'Sim' : 'não')}, porém os métodos são difirentes \n");
+
+//Calculo2.areaCirculoEstatic(raio) método estatico sendo acessado atráves  da classe.
+// Calculo2().areaCirculo(raio)  instância da classe acesando método de instância.
 }
