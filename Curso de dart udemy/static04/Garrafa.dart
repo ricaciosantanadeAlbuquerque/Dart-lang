a33ac25373f6996741c50db1b6987289f98ac349
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-04-20 01:44:58 -0300

    20-04-2023

---
+++ Curso de dart udemy/static04/Garrafa.dart
@@ -0,0 +1,14 @@
+class Garrafa {
+  static String tipoMaterial = "Vidro";
+  static int capacidade = 10;
+
+  static void encher() {
+    print("Enchendo a garrafa de ${capacidade}ML em mais um litro");
+  }
+
+  static void esvaziar() {
+    print("Esvaziando toda a garrafa de $capacidade ML ");
+  }
+
+  
+}
