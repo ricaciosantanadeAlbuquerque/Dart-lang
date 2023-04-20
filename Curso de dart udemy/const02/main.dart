4a5a86a89f2b72c7d4f36d97bcbe00d9d88ebb97
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-04-20 00:01:35 -0300

    20-04-2023

--- Curso de dart udemy/const02/main.dart
+++ Curso de dart udemy/const02/main.dart
@@ -1,3 +1,11 @@
-void main(){
-  
+import 'numeros.dart';
+
+void main() {
+  var vetor = [1, 2, 3, 4, 5];
+  Numero numero = Numero(lista: vetor);
+  numero.localizarMaiorNumero();
+  numero.localizarMenorNumero();
+  numero.calcularMedia();
+
+// numero.lista = [1,2,3,4,5,6,7,8,9,]; \ NAO É POSSÍVEL ALTERAR O VALOR DO ATRIBULTO POIS ESTA CLASSE É INMUTÁVEL
 }
--- Curso de dart udemy/const02/numeros.dart
+++ Curso de dart udemy/const02/numeros.dart
@@ -3,13 +3,15 @@
  * localizar o valor do maior elemento, menor elemento e
  * media dos elementos do vetor.
  */
+
+/// classe imutável
 class Numero {
   final List<int>
       lista; // não pode ser 'static const' porque o valor será passado pelo construtor, se fosse 'static const' teria que ser declarado e inicializado em tempo de compilação, e não em tempo de execução. traduzaindo teria que ser declarado e inicializado na classe.
   const Numero({required this.lista});
 
   void localizarMaiorNumero() {
-    var maior = 0; // começa  com o menor valor, e termina com o maior  valor da lista
+    var maior = 0; // começa  com o menor valor, e termina com o maior  valor da lista.
     for (var j in lista) {
       if (maior < j) {
         maior = j;
@@ -17,8 +19,27 @@ class Numero {
     }
     print("O  maior valor é  ${maior} ");
   }
-}
 
-void localizarMenorNumro() {
-  var menor = 100000;
+  void localizarMenorNumero() {
+    var menor = 100000; // começa  com o maior valor possível e termina com o menor valor  da lista.
+
+    for (var i in lista) {
+      if (i < menor) {
+        menor = i;
+      }
+    }
+    print("O menor valor: ${menor}");
+  }
+
+  void calcularMedia() {
+    var acumuladora = 0;
+    int cont = 0;
+
+    for (int i = 0; i < this.lista.length; i++) {
+      cont++;
+      acumuladora += lista[i]; // a cada loop o valor anterior será somado com o atual e o resultado se guardado na variável 'acumuladora'.
+    }
+    var media = acumuladora / cont;
+    print("Média: ${media} ");
+  }
 }
