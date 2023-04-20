91295230a35b40a9f3297f41dff841a9117d3248
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-04-19 23:23:07 -0300

    19-04-2023

--- Curso de dart udemy/const01/main.dart
+++ Curso de dart udemy/const01/main.dart
@@ -2,8 +2,8 @@ import '../const/Brasil.dart';
 
 void main() {
   var brasil = Brasil(-7.11532, -34.8617);
-  print("Latitude: ${brasil.latitude}");
+  print("Latitude: ${brasil.latitude}"); // atribulto de instância;
   print("Longitude:${brasil.longitude}");
-  print("Capital: ${Brasil.capital}");
+  print("Capital: ${Brasil.capital}"); // atribulto de classe;
   print("Estado: ${Brasil.estado}");
 }
