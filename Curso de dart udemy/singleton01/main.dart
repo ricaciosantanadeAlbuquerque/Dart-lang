8a0c6ac449b3f103ec79e9707a03edfb33800cac
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-04-25 03:32:42 -0300

    padrão singleton 25-04-2023

---
+++ Curso de dart udemy/singleton01/main.dart
@@ -0,0 +1,20 @@
+import 'pessoa.dart';
+
+void main() {
+  var pessoa1 = Pessoa.instancia;
+  pessoa1.nome = "Lucas";
+
+  var pessoa2 = Pessoa.instancia;
+
+  print(pessoa1.nome);
+  print(pessoa2.nome);
+
+  Pessoa pessoa3 = Pessoa.instancia;
+  pessoa3.nome = "ana";
+
+  print(pessoa1.nome);
+  print(pessoa2.nome);
+  /**
+   * Não estamos criando novas instâncias para cada variável, mas passando a mesma instância 'instância única'da classe pessoa para cada variável de mode que tanto pessoa1 quanto pessoa2 apontão  para o mesmo objeto. 
+   */
+}
---
+++ Curso de dart udemy/singleton01/pessoa.dart
@@ -0,0 +1,6 @@
+class Pessoa {
+  static final Pessoa instancia = Pessoa._construtor(); // linhas de instanciameto.
+  String? nome;
+
+  Pessoa._construtor();
+}
