1f94f286c14d196153130d13a90cef9d650cafb4
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-04-29 18:07:10 -0300

    comentários_29-04-2023

--- Curso de dart udemy/singleton07/main.dart
+++ Curso de dart udemy/singleton07/main.dart
@@ -1,3 +1,15 @@
-void main(){
-  
+import 'produto.dart';
+
+void main() {
+  var produto1 = Produto.instancia.nomeProduto = "Bicicleta";
+
+  var produto2 = Produto.instancia.nomeProduto;
+
+  print(produto2);
+  print(Produto.instancia.nomeProduto);
+  print(identical(produto1, produto2)); // como as duas variáveis  possuem o mesmo objeto, ela são iguais ' true'
+  /**
+   * Não estamos criando  novas instâncias da classe Produto(),
+   * estasmos alocado o mesmo objeto 'singlton'  que está na classe produto, dentro das variáveis produto1 e produto2
+   */
 }
