de8649264b12ba46269f53d7b4b9575290445cd5
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-05-10 18:16:43 -0300

    while

---
+++ Curso de dart udemy/revisaoGeralParte1_14/main.dart
@@ -0,0 +1,12 @@
+/**
+ * - Utilizando a estrutura de repetição while, faça um programa que escreva todos os números entre 0 e 100.
+ */
+void main() {
+  // codigo totalmente imperativo
+  int cont = 0;
+
+  while (cont <= 100) {
+    print(cont);
+    cont++;
+  }
+}
