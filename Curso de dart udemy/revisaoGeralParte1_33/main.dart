4b04fdc9beeab361846666a26c3b37e202e5d8c4
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-05-17 16:41:13 -0300

    finalizado_Function(List<int>)List<int>

---
+++ Curso de dart udemy/revisaoGeralParte1_33/main.dart
@@ -0,0 +1,48 @@
+import 'dart:math';
+
+void main() {
+  List<int> numero = [];
+
+  final lista = preencher(numero);
+
+  int resultMaior = buscarMaior(lista);
+  int resultMenor = buscarMenor(lista);
+
+  print(lista);
+  print("O menor valor $resultMenor");
+  print("E o maior valor $resultMaior");
+}
+
+List<int> preencher(List<int> p) {
+  Random random = Random();
+
+  for (var j = 0; j < 10; j++) { // 0 10
+   
+    p.add(random.nextInt(10) + 1);
+
+  }
+ 
+  return p;
+}
+
+int buscarMaior(List<int> p) {
+  int maior = 0;
+  for (var j in p) {
+    if (j > maior) {
+      maior = j;
+    }
+  }
+
+}
+Function
+int buscarMenor(List<int> p) {
+  int menor = 1000000;
+
+  for (dynamic j in p) {
+    if (j < menor) {
+      menor = j;
+    }
+  }
+
+  return menor;
+}
