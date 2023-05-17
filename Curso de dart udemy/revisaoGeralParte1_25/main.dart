6fd1b07eaba6c427611ee09e2c58a5a37cd1e3e3
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-05-16 22:10:05 -0300

    alterações

--- Curso de dart udemy/revisaoGeralParte1_25/main.dart
+++ Curso de dart udemy/revisaoGeralParte1_25/main.dart
@@ -1,12 +1,12 @@
 /**
  * . Faça um programa em que faça a conversão de Dólar para Real. Primeiro, defina uma constante chamada 
-COTACAO com o valor de 3.18. Em seguida, leia o valor em dólares, multiplique pela cotação e exiba o 
+COTACAO com o valor de 4.94. Em seguida, leia o valor em dólares, multiplique pela cotação e exiba o 
 resultado em reais. 
  */
 import 'dart:io';
 
 void main() {
-  const cotacao = 3.18;
+  const cotacao = 4.94;
   print("Digite o valor em dolar !");
   String? valorEntrada = stdin.readLineSync();
 
@@ -14,6 +14,6 @@ void main() {
 
   double real = dolar * cotacao;
 
-  print("O valor de $dolar em reais é de $real");
-  
+  print("O valor de $dolar Dolares em reais é de $real");
+
 }
