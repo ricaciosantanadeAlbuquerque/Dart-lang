4e4482c74b1565f2de37a78b5196852ee9f80a3d
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-06-04 01:52:34 -0300

    finalizada Closures

--- Curso de dart udemy/revisaoGeralParte1_50/main.dart
+++ Curso de dart udemy/revisaoGeralParte1_50/main.dart
@@ -42,8 +42,64 @@ void funcaoClosures() {
 
   var resultado3 = numerador(100);
   print(resultado3);
+  var fx = resultado3(100);
+  print(fx);
+  //================================================================
+
+  var novoObjeto = () {
+    var id = 0;
+    var objetoCriado = (String nome, descricao) {
+      return 'id: ${(++id).toString().padLeft(2, '0')}, nome: $nome, descrção: ${descricao}'; // id++ pós incremento, ++id pré incremento
+    };
+    return objetoCriado; // retornando  referência a funcao mais interna
+  };
+  var objeto = novoObjeto();
+  print(objeto);
+  var arrayClosures = [objeto('Ricacio', 'ricaciozz@gmail.com')];
+  arrayClosures.add(objeto('(083)987874721', 'Dev Flutter'));
+  arrayClosures.add(objeto('Valor', 3300.00));
+
+  for (var item in arrayClosures) {
+    print(item);
+  }
+//================================================================
+  var novoObjeto1 = () {
+    var id = 0;
+    var novoObjetoCriado1 = (String nome, descricao) {
+      return Objeto.fromMap(map: {'id': (++id).toString().padLeft(2, '0'), 'nome': nome, 'descricao': descricao});
+    };
+    return novoObjetoCriado1;
+  };
+
+  print(novoObjeto1);
+  var caixa = novoObjeto1();
+
+  List<Objeto> listaOb = [caixa('Ana', 1550), caixa('Carmem', 3500)];
+
+  listaOb.add(caixa('Lucas', 'não toma banho !'));
+
+  for (var item in listaOb) {
+    print("");
+    print(item.toString());
+    print("");
+  }
 }
 
 void main() {
   funcaoClosures();
+}
+
+//=======================================================================
+class Objeto {
+  String id, nome;
+  dynamic descricao;
+
+  Objeto({required this.nome, required this.descricao, required this.id});
+
+  factory Objeto.fromMap({required Map<String, dynamic> map}) {
+    return Objeto(nome: map['nome'], descricao: map['descricao'], id: map['id'].toString());
+  }
+
+  @override
+  String toString() => 'id:$id, nome:$nome, descrição:$descricao';
 }
