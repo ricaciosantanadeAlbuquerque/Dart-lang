c869c3226adb491681b5d2e3cf3fa9c89ad064d1
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-06-14 00:15:09 -0300

    construtor com parâmetros nomeados e default

--- Curso de dart udemy/RevisaoConstrutores12/main.dart
+++ Curso de dart udemy/RevisaoConstrutores12/main.dart
@@ -6,4 +6,6 @@ void main() {
 
   Pessoa pessoa2 = Pessoa("João", 21, cor: "Preto", altura: 1.73); // parâmetros nomeados devem ser declarados para receber  um valor
   print("\n Nome: ${pessoa2.nome}, Idade: ${pessoa2.idade}, Cor: ${pessoa2.cor} Altura:${pessoa2.altura}\n");
+
+  print("São iguais ? ${identical(pessoa1, pessoa2)? "Sim! são iguais":"Não ! são diferentes."}");
 }
