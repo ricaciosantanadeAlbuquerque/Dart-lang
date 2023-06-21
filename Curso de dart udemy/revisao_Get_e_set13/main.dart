d5fba0129967ffb7e3f230c15bc3ea87ef0ccdcf
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-06-20 23:07:42 -0300

    operador em cascata

--- Curso de dart udemy/revisao_Get_e_set13/main.dart
+++ Curso de dart udemy/revisao_Get_e_set13/main.dart
@@ -9,4 +9,13 @@ void main() {
   usuario1.senha = '123456789';
 
   print("Nome: ${usuario1.nome}, Senha:${usuario1.senha}");
+  print("======================================================");
+
+  var usuario2 = Usuario(nome: 'Lucas', senha: 'jjjR');
+  usuario2
+    ..senha = '44'
+    ..alteracao = true
+    ..senha = '55';
+
+  // Operador em cascata;
 }
