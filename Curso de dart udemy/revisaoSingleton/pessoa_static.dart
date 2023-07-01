eeb10cdaa439b4bb6d360787f09d83cbdcbc1d0c
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-06-30 23:49:46 -0300

    add comentários

--- Curso de dart udemy/revisaoSingleton/pessoa_static.dart
+++ Curso de dart udemy/revisaoSingleton/pessoa_static.dart
@@ -1,6 +1,6 @@
 class PessoaStatic {
   // atribulto de classe
-  static final PessoaStatic instancia = new PessoaStatic._construtorNomeado(); 
+  static final PessoaStatic instancia = new PessoaStatic._construtorNomeado(); // linha de instanciamento
   // atribulto de instância
   String? nome;
 
