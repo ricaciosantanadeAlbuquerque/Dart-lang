7b906b80fd4b69575e1dbc89a1a0c2420be1df24
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-07-01 02:43:09 -0300

    singletonFactory

--- Curso de dart udemy/revisaoSingleton4/pessoa_factory.dart
+++ Curso de dart udemy/revisaoSingleton4/pessoa_factory.dart
@@ -6,3 +6,7 @@ class PessoaFactory {
 
   PessoaFactory._construtorNomeado();
 }
+
+/**
+ * Se instância for null, então será criado um objeto e atribuido a instância, que por sua vez será retornada eeeee não terá mais seu * valor sobreposto ou alterado já que instancia não é mais null.
+ */
