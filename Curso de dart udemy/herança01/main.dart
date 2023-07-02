bc483a6ac1d015ca95ba7510555550e38bf2cff6
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-07-02 01:49:33 -0300

    herança super()

--- Curso de dart udemy/herança01/main.dart
+++ Curso de dart udemy/herança01/main.dart
@@ -12,19 +12,21 @@ class Animal {
 class Mamifero extends Animal {
   String? sexo;
 
-  Mamifero(this.sexo, idade, docil) : super(idade, docil);
+  Mamifero(this.sexo, idade, docil) : super(idade, docil); // super() refere-se ao construtor da classe Animal 
 
-  void Alimentar() {
+  void alimentar() {
     print('Se alimentar como um mamifero! ');
   }
 }
 
-class Cao extends Mamifero {
-  // Cao() herda tudo de Mamifero() que herda tudo de Animal(). Cao() tem tudo que Mamifero e Animal tem
+class Cao extends Mamifero { // Cao() herda tudo de Mamifero() que herda tudo de Animal(). Cao() tem tudo que Mamifero e Animal tem
   String? nome;
   String? raca;
 
-  Cao(this.nome, this.raca, String sexo, {String? idade, bool? docil}) : super(sexo, idade, docil); // super() refere-se ao construtor da super classe Mamifero! .
+  Cao(this.nome, this.raca, String? sexo, {String? idade, bool? docil = false}) : super(sexo, idade, docil) {
+    this.idade = (idade == null) ? 'indefinido' : idade;
+    this.sexo = (sexo == null) ? 'indefinido' : sexo;
+  } // super() refere-se ao construtor da super classe Mamifero! .
 
   void acao() {
     print("Late como um cão !.");
@@ -34,5 +36,12 @@ class Cao extends Mamifero {
 void main() {
   var novaCao = new Cao("Bola", 'pastor Alemão', 'macho');
 
-  print("Nome: ${novaCao.nome}, Raça: ${novaCao.raca}, Sexo:${novaCao.sexo}, Idade:${novaCao.idade}, Docil: ${novaCao.docil}");
+  print("Nome: ${novaCao.nome}, Raça: ${novaCao.raca}, Sexo: ${novaCao.sexo}, Idade: ${novaCao.idade}, Docil: ${novaCao.docil}");
+
+  novaCao
+    ..alimentar()
+    ..dorme()
+    ..acao();
+
+  print("Nome: ${novaCao.nome} Está amigável ? ${(novaCao.docil == true) ? 'SIM' : 'NAO'}");
 }
