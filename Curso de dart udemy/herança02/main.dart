11a71619a4acbe4c616484f9f3e26e75e2473306
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-07-06 17:19:33 -0300

    operdor em cascata

--- Curso de dart udemy/herança02/main.dart
+++ Curso de dart udemy/herança02/main.dart
@@ -1,7 +1,13 @@
 import 'cao.dart';
 
 void main() {
-  var cao = Cao("Max",'vira Lata', idade: 3,sexo:'Macho',docil: true);
+  var cao = Cao("Max", 'vira Lata', idade: 3, sexo: 'Macho', docil: true);
+  print(cao.toString());
 
- print(cao.toString());
+  cao
+    ..dorme()
+    ..alimentar()
+    ..acao();
+
+  print("${cao.nome} ${(cao.docil == true) ? 'está Amigável' : 'não está amigável'}");
 }
