111066cb85816a931cf8b37e8458c9636a567887
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-07-20 03:50:37 -0300

    instanciando classes

--- Curso de dart udemy/interfaces_3/main.dart
+++ Curso de dart udemy/interfaces_3/main.dart
@@ -1,3 +1,10 @@
-void main(){
+import 'cliente.dart';
+import 'fornecedor.dart';
+import 'produto.dart';
+
+void main() {
+  var produto = new Produto(1, 'Camisa');
+  var cliente = Cliente(007, 'Lucas');
+  var fornecedor = Fornecedor('106-888-9520', '000-354-77');
   
 }
