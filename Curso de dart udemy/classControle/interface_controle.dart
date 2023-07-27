f44c235fca0067d44992f7c8d6fe01f3d70f02f6
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-07-27 02:17:54 -0300

    interface do controle

---
+++ Curso de dart udemy/classControle/interface_controle.dart
@@ -0,0 +1,12 @@
+abstract class Controle {
+  void ligar();
+  void desligar();
+  void abrirMenu();
+  void fecharMenu();
+  void mainVolume();
+  void menosVolume();
+  void ligarMudo();
+  void desligarMudo();
+  void pay();
+  void pause();
+}
