1d7f8b58fbb20782bd98ca20bbe6845c126d9336
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-07-27 20:49:10 -0300

    alterações feitas

--- Curso de dart udemy/classControle/controle.dart
+++ Curso de dart udemy/classControle/controle.dart
@@ -1,82 +1,136 @@
 import 'interface_controle.dart';
 
-class Controle  implements InterfaceControle {
+class Controle implements InterfaceControle {
   int? _volume;
   bool? _ligado;
   bool? _tocando;
   // constructor
-  Controle() {
-    this.volume = 50;
-    this.ligado = false;
-    this.tocado = false;
-  }
-  // get and set
-  int? get volume => this._volume;
 
-  void set volume(int? volume) {
+  Controle() {
+    this._volume1 = 50;
+    this._ligado1 = false;
+    this._tocando1 = false;
+  }
+
+  // get and set
+  int? get _volume1 => this._volume;
+
+  void set _volume1(int? volume) {
     this._volume = volume;
   }
 
-  bool? get ligado => this._ligado;
+  bool? get _ligado1 => this._ligado;
 
-  void set ligado(bool? liagdo) {
+  void set _ligado1(bool? liagdo) {
     this._ligado = liagdo;
   }
 
-  bool? get tocando => this._tocando;
+  bool? get _tocando1 => this._tocando;
 
-  void set tocado(bool? tocando) {
+  void set _tocando1(bool? tocando) {
     this._tocando = tocando;
   }
 
   // interface
- @override
-  void ligar(){
-
-  }
- 
- @override
-  void desligar(){
-
+  @override
+  void ligar() {
+    this._ligado1 = true;
+    this._tocando1 = true;
   }
 
-@override
-void abrirMenu(){
+  @override
+  void desligar() {
+    this._ligado1 = false;
+  }
 
-}
-@override
-void fecharMenu(){
+  @override
+  void abrirMenu() {
+    if (this._ligado1 == true) {
+      // se ligado ?
 
-}
-@override
-void maisVolume(){
+      print(this._volume1); // então moste o volume
+      for (var i = 0; i < this._volume1!; i += 10) {
+        print("|");
+      }
+    } else {
+      print("ERRO! não é possível mostrar o menu !");
+    }
+  }
 
-}
-@override
-void menosVolume(){
+  @override
+  void fecharMenu() {
+    if (this._ligado1 == true) {
+      print("Menu Encerrado! ");
+    } else {
+      print("ERRO! não é possível mostrar o menu !");
+    }
+  }
 
-}
+  @override
+  void maisVolume() {
+    if (this._ligado1 == true) {
+      this._volume1 = this._volume1! + 10;
+    } else {
+      print("Erro!");
+    }
+  }
 
-@override
-void ligarMudo(){
+  @override
+  void menosVolume() {
+    if (this._ligado1 == true) {
+      // se ligado !
 
-}
+      this._volume1 = this._volume1! - 10;
 
-@override
-void  desligarMudo(){
+    } else {
+      print("ERRO!");
+    }
+  }
 
-}
+  @override
+  void ligarMudo() {
+    if (this._ligado == true) {
+      // se ligado
+      if (this._volume1! > 0) {
+        // ee o volume e maior que zero
+        this._volume1 = 0; // colocar para mudo
+      }
+    } else {
+      print("ERRO!");
+    }
+  }
 
-@override
-void pay(){
+  @override
+  void desligarMudo() {
+    if (this._ligado == true) {
+      if (this._volume1 == 0) {
+        this._volume1 = 50;
+      }
+    } else {
+      print("ERRO!");
+    }
+  }
 
-}
+  @override
+  void pay() {
+    if (this._ligado == true && this._tocando1 == false) {
+      this._tocando1 = true; // tocando
+    } else {
+      print("ERRO!");
+    }
+  }
 
-@override
+  @override
+  void pause() {
+    if (this._ligado == true && this._tocando1! == true) {
+      this._tocando1 = false; // não tocando
+    } else {
+      print("ERRO!");
+    }
+  }
 
-void pause(){
-
-}
-
-
+  @override
+  String toString() {
+    return 'Volume:$_volume1, Tocando: $_tocando1, Ligado: $_ligado1';
+  }
 }
--- Curso de dart udemy/classControle/main.dart
+++ Curso de dart udemy/classControle/main.dart
@@ -1,3 +1,50 @@
-void main(){
-  
+import 'controle.dart';
+
+void main() {
+  Controle controle = Controle();
+
+  // Todos os seus métodos get e sets, assim como os atributos da classe estão privados, ficando visivel só os métodos  que caracterizam a interface.
+
+  controle
+    ..ligar()
+    ..abrirMenu();
+
+  print("============================================================");
+
+  controle
+    ..maisVolume()
+    ..maisVolume();
+
+  print(controle.toString());
+
+  controle.abrirMenu();
+
+  print('');
+
+  controle.menosVolume();
+
+  print('');
+
+  print(controle.toString());
+
+  controle.pause();
+
+  print('');
+
+  print(controle.toString());
+
+  controle.pay();
+
+  print('');
+
+  print(controle.toString());
+
+  controle
+    ..pause()
+    ..desligar();
+
+    
+  print('');
+
+  print(controle.toString());
 }
