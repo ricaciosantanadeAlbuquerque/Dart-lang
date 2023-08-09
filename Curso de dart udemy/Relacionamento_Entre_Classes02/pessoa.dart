529125183f2e69f79a2c20b855b535bd30579321
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-08-09 18:44:06 -0300

    classe pessoa

---
+++ Curso de dart udemy/Relacionamento_Entre_Classes02/pessoa.dart
@@ -0,0 +1,29 @@
+class Pessoa {
+  String? _nome;
+  int? _idade;
+  String? _sexo;
+
+// get and set
+
+  String? get nome => this._nome;
+
+  void set nome(String? nome) {
+    this._nome = nome;
+  }
+
+  int? get idade => this._idade;
+
+  void set idade(int? idade) {
+    this._idade = idade;
+  }
+
+  String? get sexo => this._sexo;
+
+  void set sexo(String? sexo) {
+    this._sexo = sexo;
+  }
+
+  void fazerAniversario() {
+    this.idade = this.idade! + 1;
+  }
+}
