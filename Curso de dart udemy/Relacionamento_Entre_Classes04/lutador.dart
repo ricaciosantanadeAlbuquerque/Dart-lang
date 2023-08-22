f713321e4f4aaefe48be8b05b46c944e1e49a51d
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-08-21 22:31:20 -0300

    lista de objetos Lutador

--- Curso de dart udemy/Relacionamento_Entre_Classes04/lutador.dart
+++ Curso de dart udemy/Relacionamento_Entre_Classes04/lutador.dart
@@ -13,16 +13,15 @@ class Lutador implements InterfaceLutador {
   int? _empates;
 
 // construtor
-  Lutador({required String? nome, required String? nacionalidade, required int? idade, required double? altura, required double? peso, required String? categoria, required int? vitorias, required int? derrotas, required int? empates}) {
-    this._nome = (nome != null) ? nome : 'indefinido';
-    this._nacionalidade = (nacionalidade != null) ? nacionalidade : 'indefinido';
-    this._idade = (idade != null) ? idade : 0;
-    this._altura = (altura != null) ? altura : 0.0;
-    this._peso = (peso != null) ? peso : 0.0;
-    this._categoria = (categoria != null) ? categoria : 'indefinido';
-    this._vitorias = (vitorias != null) ? vitorias : 0;
-    this._derrotas = (derrotas != null) ? derrotas : 0;
-    this._empates = (empates != null) ? empates : 0;
+  Lutador({required String? nome, required String? nacionalidade, required int? idade, required double? altura, required double? peso, required int? vitorias, required int? derrotas, required int? empates}) {
+    this.nome = (nome != null) ? nome : 'indefinido';
+    this.nacionalidade = (nacionalidade != null) ? nacionalidade : 'indefinido';
+    this.idade = (idade != null) ? idade : 0;
+    this.altura = (altura != null) ? altura : 0.0;
+    this.peso = (peso != null) ? peso : 0.0;
+    this.vitorias = (vitorias != null) ? vitorias : 0;
+    this.derrotas = (derrotas != null) ? derrotas : 0;
+    this.empates = (empates != null) ? empates : 0;
   }
 
 // get and set  estilo arrow function
@@ -45,11 +44,27 @@ class Lutador implements InterfaceLutador {
 
   double? get peso => this._peso;
 
-  void set peso(double? peso) => this._peso = peso;
+  void set peso(double? peso) {
+    this._peso = peso;
+
+    this._setCategoria = null; // não  vamos usar o valor passado como parametro
+  }
 
   String? get categoria => this._categoria;
 
-  void set categoria(String? categoria) => this._categoria = categoria;
+  void set _setCategoria(String? categoria) {
+    if (this.peso! < 52.2) {
+      this._categoria = 'Inválido';
+    } else if (this.peso! <= 70.3) {
+      this._categoria = 'Leve';
+    } else if (this.peso! <= 83.9) {
+      this._categoria = 'Médio';
+    } else if (this.peso! <= 120.2) {
+      this._categoria = 'Pesado';
+    } else {
+      print("Inválido");
+    }
+  }
 
   int? get vitorias => this._vitorias;
 
--- Curso de dart udemy/Relacionamento_Entre_Classes04/main.dart
+++ Curso de dart udemy/Relacionamento_Entre_Classes04/main.dart
@@ -1,3 +1,13 @@
-void main(List<String> rgs){
-  
+
+
+import 'lutador.dart';
+
+void main(List<String> rgs) {
+  final List<Lutador> litaLutador = [
+    Lutador(nome: 'Pretty Boy', nacionalidade: 'França', idade: 31, altura: 1.75, peso: 68.9,vitorias: 11, derrotas: 2, empates: 1),
+    Lutador(nome: 'PutScript', nacionalidade:'Brasil', idade: 29, altura: 1.68, peso: 57.8, vitorias: 14, derrotas: 2, empates: 3),
+    Lutador(nome: 'SnapShadow', nacionalidade:'EUA', idade: 35, altura: 1.65, peso: 80.9, vitorias: 12, derrotas: 2, empates: 1),
+    Lutador(nome: 'Dead Code', nacionalidade: 'Austrália', idade: 28, altura: 1.93, peso: 81.6, vitorias: 13, derrotas: 0, empates: 2),
+    Lutador(nome: 'Nerdaard', nacionalidade: 'EUA', idade: 30, altura: 1.81, peso: 105.7, vitorias: 12, derrotas: 2, empates: 4),
+  ];
 }
