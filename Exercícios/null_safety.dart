3c0525791599d8fa4b18e5607892887a19a20844
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-09-02 19:33:59 -0300

    alteração

--- Exercícios/null_safety.dart
+++ Exercícios/null_safety.dart
@@ -16,7 +16,7 @@ void main() {
 
   int? num1; // num1 é uma variável que aceita valores nullos
   num1 = 19;
-  print(num1!); // ! diz para o compilador  que num1 minha variável
+  print(num1); // ! diz para o compilador  que num1 minha variável
   //nula, não é  nula ou não está nula ou seja estou falando que ela possui
   // um valor not null;
 
