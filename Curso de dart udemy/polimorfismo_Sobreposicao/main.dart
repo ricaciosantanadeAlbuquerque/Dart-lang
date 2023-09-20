6d0c1dfc11d15bd97cbff43236fca4d3a92f1596
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-09-20 16:47:53 -0300

    polimorfismo de sobreposição obrigatória

--- Curso de dart udemy/polimorfismo_Sobreposicao/main.dart
+++ Curso de dart udemy/polimorfismo_Sobreposicao/main.dart
@@ -1,3 +1,44 @@
-void main(){
-  
+import 'animal.dart';
+import 'ave.dart';
+import 'mamifero.dart';
+import 'peixe.dart';
+import 'reptil.dart';
+
+void main() {
+  // var animal = new Animal();  CLASSES ABSTRATAS NÃO PODEM SER INSTÂNCIADAS;
+
+  Mamifero mamifero = Mamifero(); // linha de Instânciamento
+  Peixe peixe = Peixe();
+  var ave = Ave();
+  var reptil = Reptil();
+
+// =====================================================================================================================
+
+  mamifero
+    ..alimentar()
+    ..locomover() // correndo
+    ..emitirSom();
+
+  print('');
+
+  peixe
+    ..alimentar()
+    ..locomover() // Nadando
+    ..emitirSom();
+
+  print('');
+
+  ave
+    ..alimentar()
+    ..emitirSom()
+    ..locomover();
+
+  print('');
+
+  reptil
+    ..alimentar()
+    ..locomover()
+    ..emitirSom();
+
+ // .. Operador em cascata ! permite evocar vários métodos ou atibultos de um mesmo objeto 
 }
