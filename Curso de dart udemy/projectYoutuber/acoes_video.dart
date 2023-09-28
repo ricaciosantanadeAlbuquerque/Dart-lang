5c4ff0a91d47172ec73e700a89ca923d3abeb8eb
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-09-28 18:03:03 -0300

    Testando a classe Video

--- Curso de dart udemy/projectYoutuber/acoes_video.dart
+++ Curso de dart udemy/projectYoutuber/acoes_video.dart
@@ -4,4 +4,8 @@ abstract class AcoesVideo {
   void pause();
   void like();
   
+  /**
+   * Classes abstract em Dart  são usadas como interface.
+   * void play(); ( Métodos abstratos são previstos  na interface, Porém são implementados, pela classe que implementa a * interface )
+   */
 }
--- Curso de dart udemy/projectYoutuber/main.dart
+++ Curso de dart udemy/projectYoutuber/main.dart
@@ -1,3 +1,11 @@
-void main(){
-  
+import 'video.dart';
+
+void main() {
+  List<Video> listaVideo = [
+    Video(title: 'Aula 1 de POO'),
+    Video(title: 'Aula 12 PHP'),
+    Video(title: 'Aula 10 HTML5'),
+  ];
+
+  print(listaVideo[0].titulo);
 }
