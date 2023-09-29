c0c78f1354665b79efac866cf242d2c00b3f1c5c
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-09-29 01:50:44 -0300

    abstract classe Pessoa1

--- Curso de dart udemy/projectYoutuber1/pessoa.dart
+++ Curso de dart udemy/projectYoutuber1/pessoa.dart
@@ -1,3 +1,36 @@
-abstract class Pessoa{
-  
+abstract class Pessoa1 { // classe modelo
+
+// atributos ==============================================================
+
+  String? _nome;
+  int? _idade;
+  String? _sexo;
+  double? _experiencia;
+// construtor ==============================================================
+  Pessoa1(String nome, int idade, String sexo) {
+    this._nome = nome;
+    this._idade = idade;
+    this._sexo = sexo;
+    this._experiencia = 0;
+  }
+
+// get and set ==============================================================
+
+  String? get nome => this._nome;
+
+  void set nome(String? nome) => this._nome = nome;
+
+  int? get idade => this._idade;
+
+  void set idade(int? idade) => this._idade = idade;
+
+  String? get sexo => this._sexo;
+
+  void set sexo(String? sexo) => this._sexo = sexo;
+
+  double? get experiencia => this._experiencia;
+
+  void set experiencia(double? experiencia) => this._experiencia = experiencia;
+
+  void ganharExp();
 }
