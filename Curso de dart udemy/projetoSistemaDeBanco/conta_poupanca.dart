f2589dade691832492e395ac93d14a587750bd1b
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-10-17 20:13:54 -0300

    classe movimento

---
+++ Curso de dart udemy/projetoSistemaDeBanco/conta_poupanca.dart
@@ -0,0 +1,11 @@
+class ContaPoupanca {
+  DateTime? _aniverSarioConta;
+
+  DateTime? get aniversarioConta => this._aniverSarioConta;
+
+  void set aniverSarioConta(DateTime? aniversarioConta) {
+    this._aniverSarioConta = aniversarioConta;
+  }
+
+  void rendaConta(DateTime,double){}
+}
--- Curso de dart udemy/projetoSistemaDeBanco/movimento.dart
+++ Curso de dart udemy/projetoSistemaDeBanco/movimento.dart
@@ -1,1 +1,30 @@
-class Movimento{}
+class Movimento {
+  int? _tipoMovimento;
+  DateTime? _dataMovimento;
+  DateTime? _horaMovimento;
+  double? _valorMovimento;
+
+  Movimento();
+
+ int? get tipoMovimento => this._tipoMovimento;
+
+ set tipoMovimento(int? value) => this._tipoMovimento = value;
+
+  get dataMovimento => this._dataMovimento;
+
+ set dataMovimento( value) => this._dataMovimento = value;
+
+  get horaMovimento => this._horaMovimento;
+
+ set horaMovimento( value) => this._horaMovimento = value;
+
+  get valorMovimento => this._valorMovimento;
+
+ set valorMovimento( value) => this._valorMovimento = value;
+
+//=============================================================
+  void registrarMovimento(int,double){}
+
+  void consultarMovimento(Date){}
+
+}
