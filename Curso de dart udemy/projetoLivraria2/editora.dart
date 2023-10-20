591ba8ef24524dad167a096fc947fd78339e4fa0
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-10-20 16:47:05 -0300

    classe Editora()

---
+++ Curso de dart udemy/projetoLivraria2/editora.dart
@@ -0,0 +1,24 @@
+class Editora {
+  String? _nome;
+  String? _email;
+
+  Editora({required String? nome, required String? email});
+
+  String? get nome => this._nome;
+
+  void set nome(String? nome) {
+    this._nome = nome;
+  }
+
+  String? get email => this._email;
+
+  void set email(String? email) {
+    this._email = email;
+  }
+
+  void consultar(){
+
+  }
+
+  
+}
