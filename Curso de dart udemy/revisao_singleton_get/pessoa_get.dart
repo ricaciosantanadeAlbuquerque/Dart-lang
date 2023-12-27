42445cb5710780c3c7d36870c5877a5b36836b32
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-12-26 23:21:44 -0300

    singleton revisao com get

---
+++ Curso de dart udemy/revisao_singleton_get/pessoa_get.dart
@@ -0,0 +1,12 @@
+class PessoaGet {
+  static final PessoaGet _instancia = PessoaGet._construtorNomeado();
+  String? _nome;
+
+  PessoaGet._construtorNomeado();
+
+  static PessoaGet get instancia {
+    return _instancia;
+  }
+
+  get nome => this.nome;
+}
