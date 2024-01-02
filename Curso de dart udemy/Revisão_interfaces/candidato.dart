4a7f180bb0c8085aa9691504bc282dcd5f7fabbb
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2024-01-02 19:34:36 -0300

    interface presidenciavel

--- Curso de dart udemy/Revisão_interfaces/candidato.dart
+++ Curso de dart udemy/Revisão_interfaces/candidato.dart
@@ -1,22 +1,33 @@
 import 'cidadao.dart';
 import 'interface_postagem.dart';
+import 'interface_presidenciavel.dart';
 
-class Candidato extends Cidadao implements Postagem {
+class Candidato extends Cidadao implements Postagem, Presidenciavel {
   String? objetivo;
 
-  Candidato({String nome = 'indefinido'}) : super(nome: nome);
+  Candidato({String nome = 'indefinido', this.ideologia,this.partido}) : super(nome: nome);
 
   @override
   void objetivosPessoais() {
     print('$nome Tem o objetivo de $objetivo');
   }
 
-  // interface postagem
+  // interface concreta postagem
   @override
   String? postagem;
 
   @override
   void escreverPostagem() {
     print('Postagem da $nome no facebook: $postagem');
+  }
+
+  //  interface  abstrata presidenciavel
+  @override
+  String? ideologia;
+  String? partido;
+
+  @override
+  void ideologiaPolitica() {
+    print('$nome é candidata com ideologia $ideologia pelo partido $partido');
   }
 }
--- Curso de dart udemy/Revisão_interfaces/main.dart
+++ Curso de dart udemy/Revisão_interfaces/main.dart
@@ -1,10 +1,11 @@
 import 'candidato.dart';
 
 void main() {
-  var politico = Candidato(nome: 'Ana');
+  var politico = Candidato(nome: 'Ana', ideologia: 'Social', partido: 'Centro');
   politico
     ..objetivo = 'Ganhar a eleição.\n'
     ..objetivosPessoais()
     ..postagem = 'Vou investir na segurança e Educação.\n'
-    ..escreverPostagem();
+    ..escreverPostagem()
+    ..ideologiaPolitica();
 }
