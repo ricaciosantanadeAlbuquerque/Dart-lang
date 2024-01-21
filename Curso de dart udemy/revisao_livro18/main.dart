d1bd94f89f9b210d8c23a9e2764b0a9dd1e67b20
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2024-01-20 22:40:49 -0300

    While

---
+++ Curso de dart udemy/revisao_livro18/main.dart
@@ -0,0 +1,12 @@
+/**
+ *  Faça um programa, utilizando while, que mostre na tela os números de 0 a 100.
+ */
+void main() {
+  
+  int cont = 0;
+  while (cont <= 100) {
+    print(cont);
+    cont++; // condição de quebra
+  }
+
+}
