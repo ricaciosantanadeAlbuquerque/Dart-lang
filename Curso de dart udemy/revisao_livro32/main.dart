84d9e71957f620869419d2fc545c05cdcdac9091
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2024-01-30 01:21:07 -0300

    passando uma função como argumento para uma outra funcao revisao

---
+++ Curso de dart udemy/revisao_livro32/main.dart
@@ -0,0 +1,38 @@
+import 'dart:io';
+
+/**
+ * Crie uma funcao  que receba um valor N inteiro, e faça uma contagem até o valor N  Exemplo N == 5 .... 12345 depois 2345 depois 345 e assim 
+ * até o valor N
+ */
+
+void executarFuncao(void Function(num) p) {
+  print(''' 
+
+    Digite
+     um 
+     Valor!
+  
+  
+  ''');
+
+  String? entradaValor = stdin.readLineSync();
+  print('');
+  if (entradaValor != null && entradaValor.isNotEmpty) {
+    num valor = num.tryParse(entradaValor) ?? 0;
+
+    p(valor);
+  }
+}
+
+void main() {
+  executarFuncao(contagem);
+}
+
+void contagem(num n) {
+  for (var j = 1; j <= n; j++) {
+    for (var i = j; i <= n; i++) {
+      print(i);
+    }
+    print('\n');
+  }
+}
