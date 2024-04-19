b65b76ec4875a97b067d3686e4fab50f92b62364
unknown <ricaciozz@gmail.com>
2024-04-19 18:27:10 -0300

    teste

--- Curso de dart udemy/listRevisao1/main.dart
+++ Curso de dart udemy/listRevisao1/main.dart
@@ -19,6 +19,7 @@ void main(){
   List<int> inteiros = [1,5,10];
   List<double> doubles = [2.5,7.25];
 
+  // ignore: unused_local_variable
   List<dynamic> listaDinamica = [];
 
 // outras maneiras de concatenar Listas
@@ -33,4 +34,28 @@ void main(){
   print( listaDinamica = [...inteiros,if(doubles is List<double>) ...doubles]);
   print('=');
   print(listaDinamica = [for(var item in numeros) item]);
+
+  test(); // chamada a função
+}
+
+
+void test(){
+  print('=======================================================');
+  List<double> doubles = [1.1,2.2,3.3,4.4,5.5];
+  List<int> valores = [12,14,16,18,20];
+
+  // ignore: unnecessary_type_check
+  List<num> num1 = [...doubles,if(valores is List<int>) ...valores];
+  print('==');
+  print(num1 = [...valores,for(var item in  doubles) item]);
+  print('==');
+  print(num1);
+
+  List<List<int>> matriz = [[1,2,3,],[4,5,6],[7,8,9]];
+var lista = matriz.expand((element) => element).toList();
+print('\n');
+print(lista);
+
+List<List<List<List<int>>>> box = [ [[],[[1],[2]],[]],[[[2]]]];
+print(box);
 }
