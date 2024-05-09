942ba196524674496fb6ce7c807857bbb3216d22
ricaciosantanadeAlbuquerque <ricaciozz@gmail.com>
2024-05-09 00:12:29 -0300

    HashSet e SplayTreeSet

--- Curso de dart udemy/set05/main.dart
+++ Curso de dart udemy/set05/main.dart
@@ -1,8 +1,16 @@
+import 'dart:collection';
+import 'dart:math';
+
 void main(List<String> args){
  linkedHashSet();
+ print('-----------------------------------------------------------');
+ hashSet();
+ print('------------------------------------------------------------');
+ splayTreeSet();
 }
 
 
+// set é uma coleção de elementos únicos e organizados entre chaves {}, que não podem ser acessados por index []
 
 void linkedHashSet(){
 
@@ -45,7 +53,54 @@ void linkedHashSet(){
  print(num2.lookup(6));  // retorna null
 }
 
+// HashSet é uma coleção de elementos únicos e desordenados agrupados entre chaves {}. Quenão podem ser acessados por index
 
 void hashSet(){
+ 
+ HashSet<String> hashSet01 = new HashSet();
+ hashSet01.add('A');
+ hashSet01.add('B');
+ hashSet01.add('C');
+ hashSet01.add('D');
 
+ print(hashSet01);
+
+HashSet<int> hashSet02 = new HashSet();
+
+hashSet02.add(1);
+hashSet02.add(2);
+hashSet02.add(3);
+
+print('');
+print(hashSet02);
+print('');
+
+HashSet<int> hashSet03 = new HashSet();
+
+hashSet03.add(11);
+hashSet03.add(22);
+hashSet03.add(33);
+
+print(hashSet03);
+// uma caracteristica da coleção HashSet é que seus elementos são desorganizados.
+}
+
+// SplayTreeSet é uma coleÇão de elementos únicos entre chaves {} que se organiza automaticamente e não aceita valores null e nem pode ser acessado por index.
+
+void splayTreeSet(){
+SplayTreeSet<int> splayTreeSet01 = new SplayTreeSet();
+
+
+// gerando valores aleatórios para  splayTreeSet01
+  for(var j = 0; j < 100; j++){
+    splayTreeSet01.add(Random().nextInt(100) + 1);
+  }
+
+  // mostrando a lista 
+  print('splayTreeSet01: ${splayTreeSet01}');
+
+  /**
+   * Como vimos a coleção SplayTreeSet tem a capacidade de se organizar por si só, já que é uma arvore de comparações.
+   * PS!!! SplayTreeSet não aceita valores null
+   */
 }
