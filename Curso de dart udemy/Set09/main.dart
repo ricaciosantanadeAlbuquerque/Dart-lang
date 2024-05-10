c0ba044e8e210b09e9f24d3fb215d87e87b8badd
ricaciosantanadeAlbuquerque <ricaciozz@gmail.com>
2024-05-10 02:30:12 -0300

    SplayTreeSet

--- Curso de dart udemy/Set09/main.dart
+++ Curso de dart udemy/Set09/main.dart
@@ -36,4 +36,26 @@ void main(List<String> args){
   print(lista.union(lista02));
   print(lista.intersection(lista02));
   print(lista.lookup(6));
+
+  lista.forEach((e) => print('forEach: $e'));
+  // ============================Set do tipo List de int
+  Set<List<int>> num1 = Set();
+  num1.add([1,2,3]);
+  num1.add([4,5,6]);
+  num1.add([7,8,9]);
+  num1.add([10,11,12]);
+  num1.add([13,14,15]);
+
+  print(num1);
+  int cont0 = 0;
+  int cont1 = 0;
+  for(var  vetor in num1){
+    cont0++;
+    print('------${cont0}');
+    vetor.forEach((e){
+      cont1++;
+      print(cont1);
+      //print(e);
+    });
+  }
 }
