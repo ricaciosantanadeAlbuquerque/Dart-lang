3b2d64c6eee8c7f1c7e0aef746093bedf45a01d7
ricaciosantanadeAlbuquerque <ricaciozz@gmail.com>
2024-05-09 23:44:43 -0300

    coleção set

--- Curso de dart udemy/set06/main.dart
+++ Curso de dart udemy/set06/main.dart
@@ -26,7 +26,31 @@ void main(List<String> args){
 
   for(var i = 0; i < listSet.length; i++){
     //print(listSet[i]); ERRO!! não é possível fazer acesso a coleções Set por index []
+    print('for ${listSet.elementAt(i)}');
   }
+
+  print('');
+
+  for(var element in listSet ){
+     print('for in ${element}');
+  }
+
+  print('');
+
+  listSet.forEach((e){
+    print('forEach ${e}');
+  });
+
+  // testando métodos da coleção set
+
+  Set<int> listNum1 =  {1,2,3,4,5};
+  Set<int> listNum2 = {4,5,6,7,8};
+
+  print(listNum1.difference(listNum2)); // difference == diferença
+  print(listNum1.union(listNum2)); // union == união
+  print(listNum1.intersection(listNum2)); // intersection == intercessão
+  print(listNum1.lookup(3)); // se o elemento existir na coleção set então ele será retornado. Se não retorna null
+  print(listNum2.lookup(10));
 }
 
 
