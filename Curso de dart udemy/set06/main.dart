5fa6547389c06895322190b11da8bb8b2bce5e89
ricaciosantanadeAlbuquerque <ricaciozz@gmail.com>
2024-05-10 00:11:35 -0300

    HashSet

--- Curso de dart udemy/set06/main.dart
+++ Curso de dart udemy/set06/main.dart
@@ -1,11 +1,14 @@
-void main(List<String> args){
-   linkedHashSet();
+import 'dart:collection';
+
+void main(List<String> args) {
+  linkedHashSet();
+  print('');
+  hashSet();
 }
 
 /// Set é uma coleção de elementos únicos e ordenados entre chaves, que Segue a ordem de inserção, e não podem ser acessados por index [].
 
- void linkedHashSet(){
-
+void linkedHashSet() {
   Set<int> listSet = Set();
   print('Tipo da coleção ${listSet.runtimeType}');
   // listSet[0] = 12; ERRO!!! Coleções set não aceitam acesso por index
@@ -14,7 +17,7 @@ void main(List<String> args){
   listSet.add(22);
   listSet.add(23);
   listSet.add(23);
-  listSet.add(20); // set é uma coleção de elementos únicos, não aceitando dados redundantes. 
+  listSet.add(20); // set é uma coleção de elementos únicos, não aceitando dados redundantes.
 
   print('listSet: ${listSet}');
 
@@ -24,27 +27,27 @@ void main(List<String> args){
 
   // for
 
-  for(var i = 0; i < listSet.length; i++){
+  for (var i = 0; i < listSet.length; i++) {
     //print(listSet[i]); ERRO!! não é possível fazer acesso a coleções Set por index []
     print('for ${listSet.elementAt(i)}');
   }
 
   print('');
 
-  for(var element in listSet ){
-     print('for in ${element}');
+  for (var element in listSet) {
+    print('for in ${element}');
   }
 
   print('');
 
-  listSet.forEach((e){
+  listSet.forEach((e) {
     print('forEach ${e}');
   });
 
   // testando métodos da coleção set
 
-  Set<int> listNum1 =  {1,2,3,4,5};
-  Set<int> listNum2 = {4,5,6,7,8};
+  Set<int> listNum1 = {1, 2, 3, 4, 5};
+  Set<int> listNum2 = {4, 5, 6, 7, 8};
 
   print(listNum1.difference(listNum2)); // difference == diferença
   print(listNum1.union(listNum2)); // union == união
@@ -53,11 +56,56 @@ void main(List<String> args){
   print(listNum2.lookup(10));
 }
 
+/// HashSet é uma coleção de elementos únicos e desordenados entre chaves {} que não podem ser acessados por index []
 
-void hashSet(){
+void hashSet() {
+  HashSet<String> hashSet01 = HashSet();
+  hashSet01.add('A');
+  hashSet01.add('B');
+  hashSet01.add('C');
 
+  print(hashSet01);
+
+  HashSet<int> hashSet02 = HashSet();
+  hashSet02.add(1);
+  hashSet02.add(2);
+  hashSet02.add(3);
+
+  print('');
+
+  print(hashSet02); // caso a coleção do tipo HashSet seja tipada como <int>  e só tenha valores com uma casa decimal, então será respeitada a ordem de inserção
+
+  HashSet<int> hashSet03 = HashSet();
+
+  hashSet03.add(11);
+  hashSet03.add(22);
+  hashSet03.add(33);
+
+  print('');
+  print(hashSet03);
+
+  for (var i = 0; i < hashSet01.length; i++) {
+    print(hashSet01.elementAt(i));
+  }
+
+  print('-----------------------------------------------------------\n');
+
+  for (var element in hashSet02) {
+    print(element);
+  }
+
+  print('-----------------------------------------------------------\n');
+
+
+  hashSet03.forEach((e) => print( e ));
+
+  print('---------------------Métodos----------------------');
+
+  print(hashSet02.difference(hashSet03));
+  print(hashSet02.union(hashSet03));
+  print(hashSet02.intersection(hashSet03));
+  print(hashSet02.lookup(5));
+  print(hashSet03.lookup(33));
 }
 
-void splayTreeSet(){
-
-}
+void splayTreeSet() {}
