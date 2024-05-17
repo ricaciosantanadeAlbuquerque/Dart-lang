dc9ac3b594cf72063a158bb1f85df4332b321362
ricaciosantanadeAlbuquerque <ricaciozz@gmail.com>
2024-05-17 01:09:12 -0300

    terinando Queue com Random()

---
+++ Curso de dart udemy/Queue11/main.dart
@@ -0,0 +1,20 @@
+import 'dart:collection';
+import 'dart:io';
+import 'dart:math';
+void main(List<String> args){
+  Queue<String> nomes = Queue();
+
+   for(var i = 0; i < 3 ; i++){
+     print('Digite seu nome');
+     String? entradaNome = stdin.readLineSync();
+     if(entradaNome != null && entradaNome.isNotEmpty){
+      nomes.add(entradaNome);
+     }else{
+      print('ERRO! Valor não informado !!');
+      exit(0);
+     }
+   }
+
+   print('O Ganhador ou Ganhadora é ${nomes.elementAt(Random().nextInt(3 + 1))}');
+
+}
