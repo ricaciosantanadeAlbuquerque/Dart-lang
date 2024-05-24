ef749b5fdf6d1b87b2bac89b9145f3fcf55780d0
ricaciosantanadeAlbuquerque <ricaciozz@gmail.com>
2024-05-24 01:07:17 -0300

    operadores

--- Curso de dart udemy/Revisao01/operadores.dart
+++ Curso de dart udemy/Revisao01/operadores.dart
@@ -43,7 +43,7 @@ void main(){
       ~/= divisão inteira igual
       %= resto da divisão igual
       ++  incremento unitário
-      --  decremento unitário
+      --  decremento unitário.
     */
 
 
