8ca89df512f23a55342d630fb1ec598bdc84d412
ricaciosantanadeAlbuquerque <ricaciozz@gmail.com>
2024-05-24 00:56:11 -0300

    operadores

---
+++ Curso de dart udemy/Revisao01/operadores.dart
@@ -0,0 +1,51 @@
+void main(){
+/**
+ * ()
+ * * / ~/ %
+ * + -
+ * >
+ * <
+ * >=
+ * <=
+ * !=
+ * ==
+ * &&
+ * ||
+ * !
+ */
+
+ double num1 = 10;
+ double num2 = 5;
+  
+   num1 += 50;
+   print(num1);
+  
+   double soma = num1 + num2;
+   double subtracao = num1 - num2;
+   double multiplicacao = num1 * num2;
+   double divisao = num1 / num2;
+   int divisaoInt = num1.toInt() ~/ num2.toInt();
+  
+  print('Soma:${soma}, Subtração ${subtracao}, multiplicação: ${multiplicacao}, divisao:${divisao} divisãoInt:${divisaoInt}');
+
+   /**
+    * + 
+      -
+      *
+      /
+      ~/
+      ()
+      %
+      +=
+      -=
+      *=
+      /=
+      ~/=
+      %=
+      ++
+      --
+    */
+
+
+
+}
