85671e053ad80f8f67a2689ef147446e68cb11b6
ricaciosantanadeAlbuquerque <ricaciozz@gmail.com>
2024-07-20 02:55:26 -0300

    Map<> reduce teste

--- Curso de dart udemy/mapRevisao09/main.dart
+++ Curso de dart udemy/mapRevisao09/main.dart
@@ -27,5 +27,22 @@ void mapReduce(){
  var algumBolsista = alunos.map(bolsista).reduce((ant,atu) => ant || atu);
 
  print('Todos são bolsistas ? ${todosBolsistas ? 'Sim' : 'Não'}\nAlgum Bolsista ? ${algumBolsista ? 'Sim' : 'Não'}');
- 
+
+ List<dynamic> funcionarios = [
+  {'nome':'João','genero':'H','pais':'Brasil','salario':1879},
+  {'nome':'Matheus','genero':'H','pais':'Brasil','salario':3500},
+  {'nome':'Ana','genero':'M','pais':'Peru','salario':5000}
+ ];
+  final brasileiro = (map) => map['pais'] == 'Brasil';
+  final homem = (map) => map['genero'] == 'H';
+ List<dynamic> listaBrasileirosHomens = funcionarios.where(brasileiro).where(homem).toList();
+ print(listaBrasileirosHomens);
+
+final menorSalario =  (ant, atu) => ant['salario'] < atu['salario'] ? ant['salario'] : atu['salario'];
+final funcionarioMenorSala = (ant,atu) => ant['salario'] < atu['salario'] ? ant : atu;
+ // qo menor salario use o reduce()
+ print('O menor salário ${listaBrasileirosHomens.reduce(menorSalario)}');
+// o funcionário com o menor salario
+ print('Funcionário: ${listaBrasileirosHomens.reduce(funcionarioMenorSala)}');
+
 }
