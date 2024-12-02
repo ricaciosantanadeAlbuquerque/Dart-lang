a99abfe1d55049b8149ed7889a4ecc01383888e4
RICACIO SANTANA <ricaciozz@gmail.com>
2024-12-01 23:58:39 -0300

    Tratando Future com try/catch() e testando Future.wait()

--- Curso de dart udemy/future19/main.dart
+++ Curso de dart udemy/future19/main.dart
@@ -1,9 +1,30 @@
-void main(List<String> args)async{
+import 'dart:io';
+
+void main(List<String> args) async {
   /// Sincrona !!!
   contagem(segundos: 5);
 
   /// Assincrona
-  print(await tarefa(tempo: 6));
+ // print(await tarefa(tempo: 6));
+
+  // Provocando erro !!
+
+  //print(await tarefa(tempo: 3));
+
+  List<Future> objectFuture = [tarefa(tempo: 6), tarefa(tempo: 5)];
+
+  print(await Future.wait(objectFuture)); 
+  /**
+   * Future.wait == Futuro esperado.
+   * Recebe uma lista de Future e retorna uma lista de resultados.
+   * 
+   *  Construtores nomeados
+   * Future.delayed()
+   * Future.value()
+   * Future.error()
+   * Future.sync()
+   * 
+   */
 }
 
 ///
@@ -21,11 +42,18 @@ void contagem({int segundos = 3}) {
   }
 }
 
-tarefa({int tempo = 3})async{
-  var result = await statu(tempo);
+Future<String> tarefa({int tempo = 3}) async {
+  var result = await statu(tempo) ?? exit(0);
   return 'Status: ${result}';
 }
 
-Future<String> statu([int tempo = 1]) {
-  return Future.delayed(Duration(seconds: tempo), () => 'Primeira Tarefa !!');
+Future<String>? statu([int tempo = 1]) {
+  try {
+    if (tempo < 4) throw Exception('Tempo insulficiente');
+    return Future.delayed(Duration(seconds: tempo), () => 'Primeira Tarefa !!');
+  } catch (e, s) {
+    print('Exceção: ${e}');
+    print('Stack Trace: ${s}');
+    return null;
+  }
 }
