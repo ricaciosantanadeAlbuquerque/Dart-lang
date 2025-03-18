82b3f56f15757a396862aa3ad1424cd487ab50b4
ricaciosantanadeAlbuquerque <ricaciozz@gmail.com>
2025-03-18 02:17:10 -0300

    revisao funcao 2025

--- Curso de dart udemy/exe007-revisao-funcao/main.dart
+++ Curso de dart udemy/exe007-revisao-funcao/main.dart
@@ -13,8 +13,24 @@ void main() {
       print(saudacao(nome: nomeEntrada));
     }
   }
+
+  print(maior(num1: 3, num2: 30));
 }
 
 String saudacao({String nome = 'Indefinido'}) {
   return 'Seja Bem vindo $nome';
+}
+
+int maior({int num1 = 0, int num2 = 0}) {
+  var result;
+  if (num1 > num2) {
+    result = num1;
+  } else if (num2 > num1) {
+    result = num2;
+  } else if (num1 == num2) {
+    print('Os valores são iguais $num1 e $num2');
+    result = num1;
+  }
+
+  return result;
 }
