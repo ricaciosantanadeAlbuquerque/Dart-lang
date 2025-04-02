1f68d3d891bf6b8f0c17537c7a15b266eb9304e3
ricaciosantanadeAlbuquerque <ricaciozz@gmail.com>
2025-04-02 00:25:29 -0300

    funcao palindroma

--- Curso de dart udemy/funcao_palindromo/main.dart
+++ Curso de dart udemy/funcao_palindromo/main.dart
@@ -1,12 +1,17 @@
+/**
+ * Implemente uma função que verifica se uma palavra ou frase é um palíndromo (exemplo: "A base do teto desaba").
+ */
+
+
 import 'dart:io';
 
-void main(List<String> rags) {
+void main(List<String> args) {
   print('Digite uma frase Palíndroma por favor');
   String? entrada = stdin.readLineSync();
 
   if (entrada != null && entrada.isNotEmpty) {
     if (isPalindromo(frase: entrada)) {
-      print('Sim á frase é palíndroma.  Normail: ${entrada}, ao contrário: ${entrada.split('').reversed.join()}');
+      print('Sim, a frase é palíndroma.  Normal: ${entrada}, ao contrário: ${entrada.split('').reversed.join()}');
     } else {
       print('Não é palíndroma. Normal: ${entrada}, ao contrário ${entrada.split('').reversed.join()}');
     }
@@ -17,7 +22,7 @@ void main(List<String> rags) {
 
 bool isPalindromo({required String frase}) {
   // Removendo espaços e pontuações e convertendo pata minusculo.
-  String fraseLimpa = frase.toLowerCase().replaceAll(RegExp(r'[^a-zA-Z]'), '');
+  String fraseLimpa = frase.toLowerCase().replaceAll(RegExp(r'[^\p{L}]',unicode: true), ''); // Remove tudo exceto letras
 
   return fraseLimpa == fraseLimpa.split('').reversed.join();
 }
