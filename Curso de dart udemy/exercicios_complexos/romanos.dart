53037b92f6d9143716361ee956f8509acc777ba0
ricaciosantanadeAlbuquerque <ricaciozz@gmail.com>
2025-04-08 01:42:59 -0300

    romano decimal

---
+++ Curso de dart udemy/exercicios_complexos/romanos.dart
@@ -0,0 +1,33 @@
+import 'dart:io';
+
+void main() => stdout.write(romanoDecimal(romano: 'xv'.toUpperCase()));
+
+int romanoDecimal({required String romano}) {
+  Map<String, dynamic> mapRomano = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000,
+  };
+
+  int total = 0;
+  int valorAnterior = 0;
+
+  for (int i = romano.length - 1; i >= 0; i--) {
+    String letra = romano[i]; //cada letra da String passada será uma chave nome mapRomano.
+    int valorAtual = mapRomano[letra]; // chave letra retorna um valor
+
+    if (valorAtual < valorAnterior) {
+      total -= valorAtual;
+    } else {
+      total += valorAtual;
+    }
+
+    valorAnterior = valorAtual;
+  }
+
+  return total;
+}
