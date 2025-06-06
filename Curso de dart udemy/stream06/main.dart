927d36943ee2e5283de4d8f913999c5575f9fc3d
ricaciosantanadeAlbuquerque <ricaciozz@gmail.com>
2025-06-06 03:51:32 -0300

    Stream().asBroadcastStream()

---
+++ Curso de dart udemy/stream06/main.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+import 'dart:math';
+
+
+void main() {
+  final Stream<int> stream = new Stream.value(120).asBroadcastStream();
+  test01(stream);
+  test02(stream);
+  test03(stream);
+}
+
+void test01(Stream<int> stream) {
+  stream.listen(
+    (v) => print(v),
+    onDone: () => print('Finalizado!'),
+    onError: (error) => print('$error'),
+  );
+}
+
+void test02(Stream<int> stream) {
+  stream.listen(
+    print,
+    onDone: () => print('Finalizado !!'),
+    onError: (error) => print(error),
+  );
+}
+
+void test03(Stream<int> stream) {
+  final StreamSubscription<int> sub = stream.listen((v) {
+    print(v);
+  });
+
+  sub.onDone(() => print('finalizado !!!'));
+  sub.onError((e) => print('Error $e'));
+}
