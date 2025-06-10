d2a4565d448674e8ee3564e47a71b0f0821bed20
ricaciosantanadeAlbuquerque <ricaciozz@gmail.com>
2025-06-10 17:11:56 -0300

    StreamController

--- Curso de dart udemy/stream07/main.dart
+++ Curso de dart udemy/stream07/main.dart
@@ -26,9 +26,17 @@ void main(List<String> rags) async {
   );
   streamController.sink.add(120);
   streamController.sink.add(2800);
-  
+
   await streamController.sink.addStream(
     Stream.fromIterable([1500, 3000, 15000]),
   );
-  await streamController.sink.close();
+
+  // =====================================================================
+
+  streamController.add(450);
+  streamController.add(750);
+  streamController.add(1250);
+
+  streamController.close();
+
 }
