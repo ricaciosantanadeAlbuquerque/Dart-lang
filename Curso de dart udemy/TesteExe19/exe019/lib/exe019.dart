907fbc543d05a3abbe92573154fc77151e1b2c2b
ricaciosantanadeAlbuquerque <ricaciozz@gmail.com>
2025-07-29 15:09:02 -0300

    comentários

--- Curso de dart udemy/TesteExe19/exe019/lib/exe019.dart
+++ Curso de dart udemy/TesteExe19/exe019/lib/exe019.dart
@@ -1,3 +1,10 @@
 String paraMaiusculo({required String texto}) {
   return texto.toUpperCase();
 }
+/** Função String paraMaiusculo(String texto)
+Deve retornar a string com letras maiúsculas
+
+Use group() para agrupar testes com entradas diferentes
+
+Use expect(..., allOf([isA<String>(), equals('TEXTO')]))
+ */
