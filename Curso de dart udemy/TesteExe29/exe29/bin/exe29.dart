95f927e078eb49d7dece6e4fb71016491e82b4f8
ricaciosantanadeAlbuquerque <ricaciozz@gmail.com>
2025-08-08 18:40:03 -0300

    setUp() tearDwon()

---
+++ Curso de dart udemy/TesteExe29/exe29/bin/exe29.dart
@@ -0,0 +1,5 @@
+import 'package:exe29/exe29.dart' as exe29;
+
+void main(List<String> arguments) {
+  print('Hello world: ${exe29.calculate()}!');
+}
---
+++ Curso de dart udemy/TesteExe29/exe29/test/exe29_test.dart
@@ -0,0 +1,18 @@
+import 'package:exe29/exe29.dart';
+import 'package:test/test.dart';
+
+void main() {
+  late List<String> nomes; // declarando
+
+  setUp(() => nomes = []); // inicializando
+  tearDown(() => nomes.clear()); // limpando os Dados
+
+  test('Adiciona nome', () {
+    nomes.add('João');
+    expect(nomes.length, greaterThan(0));
+  });
+
+  test('Começa vazio novamente', () {
+    expect(nomes, isEmpty);
+  });
+}
