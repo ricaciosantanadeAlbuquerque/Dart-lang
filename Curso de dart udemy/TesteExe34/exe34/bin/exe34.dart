df39ee2f232610ee079042069864722fc28a7800
ricaciosantanadeAlbuquerque <ricaciozz@gmail.com>
2025-08-09 02:39:49 -0300

    late

---
+++ Curso de dart udemy/TesteExe34/exe34/bin/exe34.dart
@@ -0,0 +1,5 @@
+import 'package:exe34/exe34.dart' as exe34;
+
+void main(List<String> arguments) {
+  print('Hello world: ${exe34.calculate()}!');
+}
---
+++ Curso de dart udemy/TesteExe34/exe34/test/exe34_test.dart
@@ -0,0 +1,16 @@
+import 'package:test/test.dart';
+
+late Map<String, int> usuarios;
+void main() {
+  setUp(() {
+    usuarios = {'ana': 10, 'joao': 9};
+  });
+
+  test('Usuário existe', () {
+    expect(usuarios.containsKey('ana'), isTrue);
+  });
+
+  test('Nota correta', () {
+    expect(usuarios['joao'], equals(9));
+  });
+}
