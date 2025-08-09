dce78c3ddaee8115504876e40594e83720842b7d
ricaciosantanadeAlbuquerque <ricaciozz@gmail.com>
2025-08-09 16:39:48 -0300

    Trabalhando com late  test() e expect()

---
+++ Curso de dart udemy/Revisao_Test02/exe02/bin/exe02.dart
@@ -0,0 +1,5 @@
+import 'package:exe02/exe02.dart' as exe02;
+
+void main(List<String> arguments) {
+  print('Hello world: ${exe02.calculate()}!');
+}
---
+++ Curso de dart udemy/Revisao_Test02/exe02/test/exe02_test.dart
@@ -0,0 +1,19 @@
+import 'package:exe02/exe02.dart';
+import 'package:test/test.dart';
+
+void main() {
+  late List<String> nomes; // passo 0
+
+  setUp(() => nomes = []); // passo 1
+  tearDown(() => nomes.clear()); // passo 3
+
+  test('Adiciona nome', () {
+    // passo 2.0
+    nomes.add('João');
+    expect(nomes.length, equals(1));
+  });
+
+  test('Começa vazio novamente', () { // passo 2.1
+    expect(nomes, isEmpty);
+  });
+}
