86db954a6d18c289a2a548ee4134bad7ade01f67
ricaciosantanadeAlbuquerque <ricaciozz@gmail.com>
2025-08-13 19:00:50 -0300

    lessThanOrEqualTo

---
+++ Curso de dart udemy/Revisao_Test15/exe15/bin/exe15.dart
@@ -0,0 +1,5 @@
+import 'package:exe15/exe15.dart' as exe15;
+
+void main(List<String> arguments) {
+  print('Hello world: ${exe15.calculate()}!');
+}
---
+++ Curso de dart udemy/Revisao_Test15/exe15/test/exe15_test.dart
@@ -0,0 +1,29 @@
+import 'package:exe15/exe15.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('greaterThanOrEqualTo', () {
+    test('Exemplo greaterThanOrEqualTo', () {
+      expect(10, greaterThanOrEqualTo(10)); // true
+      expect(15, greaterThanOrEqualTo(10)); // true
+    });
+
+    test('greaterThanOrEqualTo()', () {
+      expect(200, greaterThanOrEqualTo(200)); // true
+      expect(400, greaterThanOrEqualTo(350)); // true
+    });
+  });
+
+  group('lessThanOrEqualTo', () {
+    test('lessThanOrEqualTo', () {
+      expect(5, lessThanOrEqualTo(5)); // true
+      expect(6, lessThanOrEqualTo(4)); // false
+      expect(15, lessThanOrEqualTo(20));
+    });
+  });
+}
+
+
+/**
+ * 
+ */
