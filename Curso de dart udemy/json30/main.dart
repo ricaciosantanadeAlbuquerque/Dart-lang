b42afad8e79c03eca98ebc71c96e5960acfadb39
ricaciosantanadeAlbuquerque <ricaciozz@gmail.com>
2026-04-29 03:44:53 -0300

    DECODE e ENCODE Json com construtor de lista :this()

--- Curso de dart udemy/json30/main.dart
+++ Curso de dart udemy/json30/main.dart
@@ -1,3 +1,59 @@
-void main(){
-  
+import 'dart:convert';
+
+void main() {
+  String jsonData = '''
+
+      [
+      {"nome":"biza","idade":55,"email":"biza@gmail.com"},
+      {"nome":"dora","idade":36,"email":"dora@gmail.com"},
+      {"nome":"ana","idade":44,"email":"ana@gmail.com"}
+      ]
+''';
+
+  //? Decode =====================================================================
+
+  List<dynamic> parsedJson = jsonDecode(jsonData);
+
+  ListUsuario listUsuario = new ListUsuario.fromJson(parsedJson);
+
+  print('Nome: ${listUsuario.list.elementAt(0).nome}');
+
+  Usuario usuario = new Usuario.fromJson({
+    'nome': 'Martha',
+    'idade': 42,
+    'email': 'marthajp@gmail.com',
+  });
+
+  listUsuario.list.add(usuario);
+
+  //? ENCODE
+  List<dynamic> lista = listUsuario.toJson();
+  String toJson = jsonEncode(lista);
+  print('toJson $toJson');
+}
+
+class ListUsuario {
+  late List<Usuario> list;
+
+  ListUsuario(this.list);
+
+  ListUsuario.fromJson(List<dynamic> lista)
+    : this(lista.map((e) => Usuario.fromJson(e)).toList());
+
+  List<dynamic> toJson() => this.list;
+}
+
+class Usuario {
+  late String nome;
+  late int idade;
+  late String email;
+
+  Usuario(this.nome, this.idade, this.email);
+
+  Usuario.fromJson(Map<String, dynamic> map)
+    : this(map['nome'], map['idade'], map['email']);
+
+  Map<String, dynamic> toJson() {
+    return {'nome': nome, 'idade': idade, 'email': email};
+  }
 }
